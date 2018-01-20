7455da487953a9d469efd87e5e93060a0fefe231
Anthony Carrick <anthonix@gmail.com>
2018-01-21 09:28:05 +1000

    added a datetime formatting test

--- tests.py
+++ tests.py
@@ -2,3 +2,8 @@ def t(xs):
     return [[xs[i][j] for i in range(len(xs))] for j in range(len(xs[0]))]
 
 y = t([[1,2,3],[4,5,6]])
+
+
+
+from datetime import datetime
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
